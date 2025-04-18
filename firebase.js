7da701327712ba048bfc4d16e871eb6ec72ce5a0
Ayman Alabbasi <84172150+AymanDevOps@users.noreply.github.com>
2025-04-18 12:46:04 -0400

    test data

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);       
+const auth = getAuth(app);          
+
+export { db, auth };
